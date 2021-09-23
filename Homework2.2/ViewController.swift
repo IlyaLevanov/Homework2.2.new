daef60d0b2a92202c513655a4a8975426dd67570
Ilya Levanov <ilya.levanov@inbox.ru>
2021-09-23 14:05:46 +0300

    Initial Commit

---
+++ Homework2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2.2
+//
+//  Created by Ilya on 23.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
