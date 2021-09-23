8a6e3d72e4653df1607b21923d257ed31cb365fd
Ilya Levanov <ilya.levanov@inbox.ru>
2021-09-23 16:56:32 +0300

    changing colors implemented, task is done

--- Homework2.2/ViewController.swift
+++ Homework2.2/ViewController.swift
@@ -19,23 +19,35 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     @IBOutlet var viewRGB: UIView!
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
         viewRGB.layer.cornerRadius = 10
-        
+        setColor()
     }
 
     @IBAction func redSliderAction() {
         redValueLabel.text = String(round(redSlider.value  * 100) / 100)
+        setColor()
     }
     
     @IBAction func greenSliderAction() {
         greenValueLabel.text = String(round(greenSlider.value  * 100) / 100)
+        setColor()
+
     }
     
     @IBAction func blueSliderAction() {
         blueValueLabel.text = String(round(blueSlider.value  * 100) / 100)
+        setColor()
+    }
+    
+    private func setColor(){
+        viewRGB.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                          green: CGFloat(greenSlider.value),
+                                          blue: CGFloat(blueSlider.value),
+                                          alpha: 1)
     }
 }
 
