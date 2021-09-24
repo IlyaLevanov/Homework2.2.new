9035231a77dca6701ed6a71b1a1a1f4565e6ac0b
Ilya Levanov <ilya.levanov@inbox.ru>
2021-09-24 12:44:35 +0300

    minor improvements

--- Homework2.2/ViewController.swift
+++ Homework2.2/ViewController.swift
@@ -9,15 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var redLabel: UILabel!
-    @IBOutlet var greenLabel: UILabel!
-    @IBOutlet var blueLabel: UILabel!
     @IBOutlet var redValueLabel: UILabel!
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
+    
     @IBOutlet var viewRGB: UIView!
         
     
@@ -25,19 +24,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         viewRGB.layer.cornerRadius = 10
         setColor()
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
     }
 
     @IBAction func redSliderAction() {
         redValueLabel.text = String(round(redSlider.value  * 100) / 100)
         setColor()
     }
-    
     @IBAction func greenSliderAction() {
         greenValueLabel.text = String(round(greenSlider.value  * 100) / 100)
         setColor()
-
     }
-    
     @IBAction func blueSliderAction() {
         blueValueLabel.text = String(round(blueSlider.value  * 100) / 100)
         setColor()
